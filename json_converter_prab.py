7926420d81f9fa406f210e31044379fe6f9398a2
linggom <linggom@ymail.com>
2015-08-25 19:54:30 +0700

    clean json output

--- json_converter_prab.py
+++ json_converter_prab.py
@@ -8,6 +8,9 @@ jsonfile = open('word_prab_score2.json', 'w')
 
 #fieldnames = ("word","count","avg_score")
 reader = csv.DictReader( csvfile)
+json.dump('{\"name\": \"flare\",\"children\": [', jsonfile)
 for row in reader:
-    json.dump(row, jsonfile)
-    jsonfile.write('\n')
+    if int(row['size']) > 0 :
+        json.dump(row, jsonfile)
+        jsonfile.write(',\n')
+json.dump(']}', jsonfile)
